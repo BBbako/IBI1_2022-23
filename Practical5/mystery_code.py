503a483b674d61810377172ebed74c7983b169a0
BBbako <127280602+BBbako@users.noreply.github.com>
2023-03-15 14:08:24 +0800

    Update mystery_code.py

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:
+# Answer:get 10 random number and print the maximum of them.
 
 # Import libraries
 # randint allows drawing a random number,
@@ -9,12 +9,14 @@ from random import randint
 # ceil takes the ceiling of a number, i.e. the next higher integer.
 # e.g. ceil(4.2)=5
 from math import ceil
-
+#give initial value to progress and stored_random_number.
 progress=0
 stored_random_number=0
 while progress<10:
 	progress+=1
+	#give n a value between 1 to 100
 	n = randint(1,100)
+	#store the bigger value
 	if n > stored_random_number:
 		stored_random_number = n
 
