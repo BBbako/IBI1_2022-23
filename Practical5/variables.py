f2cdb131289b0590d9c31f9ee200ef0f1d78dc11
BBbako <127280602+BBbako@users.noreply.github.com>
2023-03-15 14:52:21 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,9 +1,13 @@
-a=-3.19 #Store the longitude of Edinburgh
-b=-118.24 #Store the longitude of Los Angeles
-c=116.39 #Store the longitude of Haining
-d=abs(a-b) #Caculate the distance between Los Angeles and Edinburgh
-e=abs(a-c) #Caculate the distance between Haining and Edinburgh
-if (d>e): #Compare d and e which is bigger
+#Store the longitude of Edinburgh，Los Angeles and Haining.
+a=-3.19  
+b=-118.24 
+c=116.39 
+#Caculate the distance between Los Angeles and Edinburgh
+d=abs(a-b) 
+#Caculate the distance between Haining and Edinburgh
+e=abs(a-c) 
+#Compare d and e which is bigger
+if (d>e): 
  print("d is greater,Rob travelled further to Los Angeles.")
 else:
  print("e is greater,Rob travelled further to Haining")
