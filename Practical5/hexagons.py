c906e808ec4556cd0d9aa1b7ac6e813d8494f726
BBbako <127280602+BBbako@users.noreply.github.com>
2023-03-15 13:48:19 +0800

    Create hexagons.py

---
+++ Practical5/hexagons.py
@@ -0,0 +1,7 @@
+h=0#give an initial value to h
+n=1
+while (n<=5): #create circulate for n
+ h=2*n*(2*n-1)/2 #caculate hexagon sequence
+ n=n+1
+ print(h) #print values of hexagon sequence
+print("end") #show it is the end of the circulate
