f8bd92b341a3190a1a503559e27cd85b405145ad
BBbako <127280602+BBbako@users.noreply.github.com>
2023-03-15 14:28:14 +0800

    Create breed_like_rabbits.py

---
+++ Practical5/breed_like_rabbits.py
@@ -0,0 +1,9 @@
+#store the number of rabbit
+n=4
+#store the number of generation
+g=2
+#use a circulate to caculate the generation
+while(n<=100):
+    n=n*2
+    g=g+1
+print("100 rabbits have been born at "+ str(g)+" generation.")
