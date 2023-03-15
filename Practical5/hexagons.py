4479a4dfc2a1a9aaee3e8f7148fdfbc7c251c626
BBbako <127280602+BBbako@users.noreply.github.com>
2023-03-15 15:02:35 +0800

    Update hexagons.py

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,7 +1,12 @@
-h=0#give an initial value to h
+#pseudocode
+#   n=n+1 h=2*n*(2*n-1)/2
+#   what's the value of n?
+#   If less than 5: keep n=n+1
+#   If n=5: DONE!
+h=0
 n=1
-while (n<=5): #create circulate for n
- h=2*n*(2*n-1)/2 #caculate hexagon sequence
+while (n<=5): 
+ h=2*n*(2*n-1)/2 
  n=n+1
- print(h) #print values of hexagon sequence
-print("end") #show it is the end of the circulate
+ print(h) 
+ 
