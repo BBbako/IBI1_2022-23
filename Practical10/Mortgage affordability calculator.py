58b0bfda142fc0c11a6630b569d4e2591da07df9
BBbako <127280602+BBbako@users.noreply.github.com>
2023-04-18 18:30:42 +0800

    Create Mortgage affordability calculator.py

---
+++ Practical10/Mortgage affordability calculator.py
@@ -0,0 +1,10 @@
+# a is the total value of the house, b is the purchaser’s annual salary.
+def mac(a,b):
+    if a > b*5:
+        print("No")
+    else:
+        print("Yes")
+# example.
+a = 180000
+b = 35000
+mac(a,b)
